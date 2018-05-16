46707057cd689f48ff8c11c67eb8a60a4a52d694
Faxiang Li <lifa0917@gmail.com>
2018-05-16 15:36:44 -0400

    The bug fixed version

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
 //Load routes
-const ideas = require('./routes/ideas');
-const users = require('./routes/users');
+const ideas = require("./routes/ideas");
+const users = require("./routes/users");
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //DB Config
-const db = require('./config/database');
+const db = require("./config/database");
 
 //connect to mongoose
-mongoose.connect(db.mongoURI).then(()=>console.log('MongoDB Connected....')).catch(err=>console.log(err));
+mongoose
+  .connect(db.mongoURI)
+  .then(() => console.log("MongoDB Connected...."))
+  .catch(err => console.log(err));
 
 //Handlebars middleware
-app.engine('handlebars',exphbs({defaultLayout:'main'}
-));
-app.set('view engine','handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Body parser middleware
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Method override middleware
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 //Express session middleware
-app.use(session({
-  secret:'secret',
-  resave:true,
-  saveUninitialized:true
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 //passport middleware
 app.use(passport.initialize());
@@ -53,34 +57,33 @@ app.use(passport.session());
 app.use(flash());
 
 //Global variables
-app.use((req,res,next)=>{
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   res.locals.user = req.user || null;
   next();
 });
 
 //Index Route
-app.get('/',(req,res)=>{
-  const title = 'Welcome';
-  res.render('index',{
-    title:title
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", {
+    title: title
   });
 });
 
 //about Route
-app.get('/about',(req,res)=>{
-  res.render('About');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 //Use routes
-app.use('/ideas',ideas);
-app.use('/users',users);
+app.use("/ideas", ideas);
+app.use("/users", users);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
